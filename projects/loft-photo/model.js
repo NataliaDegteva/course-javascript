65b1a54f307636f837f2714559f2dbcb64be5d5b
Natalia Degteva <degteva_n@list.ru>
2023-08-20 22:08:36 +0300

    week 5

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -12,11 +12,95 @@ export default {
     const index = parseInt(Math.random() * (array.length - 1));
     return array[index];
   },
-  getNextPhoto() {
-    const friend = this.getRandomElement(friendsDB);
-    const photos = photosDB[friend.id];
-    const photo = this.getRandomElement(photos);
 
-    return {friend, url: photo.url};
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await.this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
+
+    return {friend, id: photo.id, url: size.url};
   },
+
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+        return biggest;
+      }, photo.sizes[0]);
+    }
+    return size;
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
+
+
+login() {
+  return new Promise((resolve, reject) => {
+    VK.init({
+      apiID: APP_ID,
+    });
+
+    VK.Auth.login((response) => {
+      if (response.session) {
+        resolve(response);
+      } else {
+        console.error(response);
+        reject(response)
+      }
+    }, PERM_FRIENDS | PERM_PHOTOS);
+  })
+},
+
+callApi(method, params) {
+  params.v = params.v || '5.120';
+
+return new Promise((resolve, reject) => {
+  VK.api(method, params, (response) => {
+    if (response.error) {
+      reject(new Error(response.error.error_msg));
+    } else {
+      resolve(response.response);
+    }
+  });
+});
 };
+
+
+getFriends() {
+  const params = {
+  fields: ['photo_50', 'photo_100'],}
+}
+return this.callApi('friends.get', params);
+
+
+
+getPhotos(owner){
+  const params = {
+    owner_id: owner,
+  };
+  return this.callApi('photos.getAll', params);
+},
+
+async getFriendPhotos(id) {
+  const photos = this.photoCache[id];
+
+  if (photos) {
+    return photos;
+  }
+
+  photos = await this.getPhotos(id);
+  this.photoCache[id] = photos;
+
+  return photos;
+},
+}
+
